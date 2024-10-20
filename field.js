8388adbc8e0ca1672ecd11f4dd06e9f2ae5d7a61
Philip Venkov <philip.venkov@gmail.com>
2024-10-20 12:32:50 -0700

    simplifiedt

--- field.js
+++ field.js
@@ -2,7 +2,7 @@ class Field {
     
     constructor() {
         this.blobs = [];
-        for (i = 0; i < random(2,5); i++) this.blobs.push(new Blob(random(0, 100), random(0, 100)));
+        for (let i = 0; i < random(2,5); i++) this.blobs.push(new Blob(random(0, 100), random(0, 100)));
         this.img = createImage(100,100);
     }
 
@@ -15,25 +15,27 @@ class Field {
     render(fieldX,fieldY) {
         this.img.loadPixels();
 
-        for (x = 0; x < 100; x++) {
-            for (y = 0; y < 100; y++) {
-            let sum = 4;
-            for (i = 0; i < this.blobs.length; i++) {
-                let xdif = x - this.blobs[i].x;
-                let ydif = y - this.blobs[i].y;
-                let d = sqrt((xdif * xdif) + (ydif * ydif));
-                sum += 15 * this.blobs[i].r / d;
-            }
-            this.img.set(x, y, color(sum, 255, 255));
+        for (let x = 0; x < 100; x++) {
+            for (let y = 0; y < 100; y++) {
+                let sum = 4;
+                for (let i = 0; i < this.blobs.length; i++) {
+                    let xdif = x - this.blobs[i].x;
+                    let ydif = y - this.blobs[i].y;
+                    let d = sqrt((xdif * xdif) + (ydif * ydif));
+                    sum += 15 * this.blobs[i].r / d;
+                }
+                this.img.set(x, y, color(sum, 255, 255));
             }
         }
         
 
-        for (i = 0; i < blobs.length; i++) {
+        for (let i = 0; i < this.blobs.length; i++) {
             this.blobs[i].update();
         }
 
         this.img.updatePixels();
+        this.img.filter(GRAY)
+        this.img.filter(THRESHOLD, thresh)
 
         image(this.img,fieldX,fieldY);
     }
--- sketch.js
+++ sketch.js
@@ -3,76 +3,27 @@
 
 // MetaBalls : p5.js implementation
 
-var blobs = []
 let testField;
+let thresh = 0.2;
 
 function setup() {
   createCanvas(600, 600);
 
   colorMode(HSB);
   
-  for (i = 0; i < random(2,5); i++) blobs.push(new Blob(random(0, 100), random(0, 100)));
   testField = new Field();
   
   frameRate(10);
   
 }
 
-function reset() {
-  background(51);
-  for (i = 0; i < random(2,5); i++) blobs.push(new Blob(random(0, 100), random(0, 100)));
-
-}
-
-function mouseClicked() {
-  // reset();
-}
-
 function draw() {
   background(51);
-  
-  let img = createImage(100,100);
-  
-  img.loadPixels();
-
-  for (x = 0; x < 100; x++) {
-    for (y = 0; y < 100; y++) {
-      let sum = 4;
-      for (i = 0; i < blobs.length; i++) {
-        let xdif = x - blobs[i].x;
-        let ydif = y - blobs[i].y;
-        let d = sqrt((xdif * xdif) + (ydif * ydif));
-        sum += 15 * blobs[i].r / d;
-      }
-      img.set(x, y, color(sum, 255, 255));
-    }
-  }
-  
-
-  for (i = 0; i < blobs.length; i++) {
-    blobs[i].update();
-  }
-
-  
-  img.updatePixels();
-  
   testField.render(200,100);
-  
-  
-  
 
-  image(img,100,100);
-  filter(GRAY);
-  // image(img1,200,100);
-  
-  
-
-  
-  
-  
-  // filter(THRESHOLD,.25);
-  
-
+  //thresh = thresh < 1 ? thresh + 0.005 : 0;
+  //filter(GRAY);
+  //filter(THRESHOLD, thresh)
 }
 
 
