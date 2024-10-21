1c82c6e2d6795f54eb427dba9b8e56e53b73cd97
Philip Venkov <philip.venkov@gmail.com>
2024-10-21 15:03:56 -0700

    working nested for loop for 3x3 grid (not all unique)

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@
 
 let testField;
 let thresh = 0.2;
+var fieldArray = [];
 
 function setup() {
   createCanvas(600, 600);
@@ -19,25 +20,37 @@ function setup() {
   testField6 = new Field();
   testField7 = new Field();
   testField8 = new Field();
+  testField9 = new Field();
   
+  fieldArray = [testField1, testField2, testField3, testField4, testField5, testField6, testField7, testField8, testField9]
   
-  
-  frameRate(8);
+  frameRate(7);
   
 }
 
 function draw() {
   background(51);
-  testField1.render(100,100);
-  testField2.render(200,100);
-  testField3.render(300,100);
-  testField4.render(400,100);
+  // testField1.render(100,100);
+  // testField2.render(200,100);
+  // testField3.render(300,100);
+  // testField4.render(400,100);
 
   
+for (var i = 0; i < 3; ++i) {
+  for (var y = 0; y < 3; ++y) {
+    fieldArray[y+i].render (150+100*i,150+100*y);
+  }
+}
 
   // thresh = thresh < 1 ? thresh + 0.005 : 0;
   // filter(GRAY);
   // filter(THRESHOLD, thresh)
+
+
+  // fill(255);
+  // strokeWeight(0);
+  // square(0,0,150);
+  // square(450,0,150);
 }
 
 function mouseClicked() {
