c98e3dd6acc1344fe4cadeeb88b5a98910cb0af9
Philip Venkov <philip.venkov@gmail.com>
2024-10-22 22:45:02 -0700

    implement random grid  + max 4x4

--- sketch.js
+++ sketch.js
@@ -6,6 +6,14 @@
 let testField;
 let thresh = 0.2;
 var fieldArray = [];
+var countArray = [1,2,3,4];
+
+var gridColumnCount;
+var gridRowCount;
+
+var gridOffsetX;
+var gridOffsetY;
+
 
 
 function setup() {
@@ -22,8 +30,36 @@ function setup() {
   testField7 = new Field();
   testField8 = new Field();
   testField9 = new Field();
+  testField10 = new Field();
+  testField11 = new Field();
+  testField12 = new Field();
+  testField13 = new Field();
+  testField14 = new Field();
+  testField15 = new Field();
+  testField16 = new Field();
   
-  fieldArray = [testField1, testField2, testField3, testField4, testField5, testField6, testField7, testField8, testField9]
+  fieldArray = [
+    testField1, 
+    testField2, 
+    testField3, 
+    testField4, 
+    testField5, 
+    testField6, 
+    testField7, 
+    testField8, 
+    testField9,
+    testField10,
+    testField11,
+    testField12,
+    testField13,
+    testField14,
+    testField16,
+    testField15,
+  ];
+  
+  gridColumnCount = random(countArray);
+  gridRowCount = random(countArray);
+  
   
   frameRate(12);
   
@@ -31,13 +67,16 @@ function setup() {
 
 function draw() {
   let arrayNumber = 0;
+  gridOffsetX = ((600-(gridColumnCount*100))/2);
+  gridOffsetY = ((600-(gridRowCount*100))/2);
+
   background(93);
   
 
   
-for (var gridY = 0; gridY < 3; ++gridY) {
-  for (var gridX = 0; gridX < 3; ++gridX) {
-    fieldArray[arrayNumber].render (150+100*gridY,150+100*gridX);
+for (var gridX = 0; gridX < gridColumnCount; ++gridX) {
+  for (var gridY = 0; gridY < gridRowCount; ++gridY) {
+    fieldArray[arrayNumber].render (gridOffsetX+(100*gridX),gridOffsetY+(100*gridY));
     arrayNumber = arrayNumber + 1;
   }
 }
@@ -54,15 +93,35 @@ for (var gridY = 0; gridY < 3; ++gridY) {
 }
 
 function mouseClicked() {
-  testField1.reset();
-  testField2.reset();
-  testField3.reset();
-  testField4.reset();
-  testField5.reset();
-  testField6.reset();
-  testField7.reset();
-  testField8.reset();
-  testField9.reset();
+  fieldArray.map((field) => field.reset());
+  {/* old way
+    testField1.reset();
+    testField2.reset();
+    testField3.reset();
+    testField4.reset();
+    testField5.reset();
+    testField6.reset();
+    testField7.reset();
+    testField8.reset();
+    testField9.reset();
+    testField10.reset();
+    testField11.reset();
+    testField12.reset();
+    testField13.reset();
+    testField14.reset();
+    testField15.reset();
+    testField16.reset(); 
+  */}
+  
+
+  gridVariablesReset();
+
+}
+
+function gridVariablesReset() {
+
+  gridColumnCount = random(countArray);
+  gridRowCount = random(countArray);
 
 }
 
